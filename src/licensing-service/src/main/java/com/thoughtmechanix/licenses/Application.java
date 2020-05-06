5ba2da864670c34f5863c3a3ccdf50354ee0cd2f
Jason Sipula <alupis1@gmail.com>
2020-05-05 23:33:53 -0700

    it lives...

--- src/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ src/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,8 +1,13 @@
 package com.thoughtmechanix.licenses;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
 }
