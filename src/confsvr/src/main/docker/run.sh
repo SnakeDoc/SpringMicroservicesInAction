57d3a3ca742d8a9ecbb34db25d494e6f97ae4f41
Jason Sipula <alupis1@gmail.com>
2020-05-07 23:37:04 -0700

    use docker

---
+++ src/confsvr/src/main/docker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "********************************************************************************"
+echo "Starting Configuration Server"
+echo "********************************************************************************"
+java -jar /usr/local/configserver/@project.build.finalName@.jar
