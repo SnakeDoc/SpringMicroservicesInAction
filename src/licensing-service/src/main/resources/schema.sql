047e3b6447df4bbd283fbcc9c512e59f1775863d
Jason Sipula <alupis1@gmail.com>
2020-05-20 00:58:51 -0700

    add base sql data for services so we have something to query out. borrowed from the book

---
+++ src/licensing-service/src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS licenses;
+
+CREATE TABLE licenses (
+  license_id        VARCHAR(100) PRIMARY KEY NOT NULL,
+  organization_id   TEXT NOT NULL,
+  license_type      TEXT NOT NULL,
+  product_name      TEXT NOT NULL,
+  comment           VARCHAR(100)
+);
+
+
+INSERT INTO licenses (license_id,  organization_id, license_type, product_name)
+VALUES ('f3831f8c-c338-4ebe-a82a-e2fc1d1ff78a', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'user','CustomerPro');
+INSERT INTO licenses (license_id,  organization_id, license_type, product_name)
+VALUES ('t9876f8c-c338-4abc-zf6a-ttt1', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'user','suitability-plus');
+INSERT INTO licenses (license_id,  organization_id, license_type, product_name)
+VALUES ('38777179-7094-4200-9d61-edb101c6ea84', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'user','HR-PowerSuite');
+INSERT INTO licenses (license_id,  organization_id, license_type, product_name)
+VALUES ('08dbe05-606e-4dad-9d33-90ef10e334f9', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'core-prod','WildCat Application Gateway');
