379874bc30d6dff18fb2c308eb4e9a037f765657
Jason Sipula <alupis1@gmail.com>
2020-05-20 00:09:49 -0700

    add bean to inject Ribbon infused RestTemplates everywhere it's autowired

--- src/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ src/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -3,8 +3,11 @@ package com.thoughtmechanix.licenses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +17,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 	
 }
